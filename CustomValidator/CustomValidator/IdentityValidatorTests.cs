5e6ebdbc16f383f6fbc1d34f570f566b4991d676
Ilya <45492035+ILYAPAP0U@users.noreply.github.com>
2018-12-05 13:19:37 +0300

    Add files via upload

---
+++ CustomValidator/CustomValidator/IdentityValidatorTests.cs
@@ -0,0 +1,17 @@
+﻿using NUnit.Framework;
+using System;
+namespace CustomValidator
+{
+    [TestFixture]
+    public class IdentityValidatorTests
+    {
+        [Test]
+        public void NameShoudNotBeNull_ValidateTest()
+        {
+            var validator = new IdentityValidator();
+            var expected = true;
+            var actual = validator.Validate(null);
+            Assert.AreEqual(expected, actual);
+        }
+    }
+}
